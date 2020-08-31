02d9e8234b86a7b8b62f8ab50a31b704fa87cc9b
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-08-31 16:17:14 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            int countOutput;
+
+            Console.Write("Введите любое слово или текст : ");
+            message = Console.ReadLine();
+
+            Console.Write("Сколько раз повторить ваше сообщение? : ");
+            countOutput = Convert.ToInt32(Console.ReadLine());
+
+            while (countOutput-- > 0)
+            {
+                Console.WriteLine(message);
+            }            
+            Console.ReadKey();
+        }
+    }
+}
